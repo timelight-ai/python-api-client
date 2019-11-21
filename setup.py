480d3b8cd5aeae207e287c86bb7ee3670a2081fd
Clément Prévost <prevostclement@gmail.com>
2019-11-21 15:41:48 +0100

    Publish using Github Actions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "timelight-ai-python-api-client"
-VERSION = "1.0.2"
+VERSION = "1.0.4"
 # To install the library, run the following
 #
 # python setup.py install
